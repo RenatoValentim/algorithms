b01096b662f5558796f6437122fa08ae92272602
RenatoValentim <rvrento28@gmail.com>
2024-04-20 02:43:27 -0300

    feat(recursive/binarysearch): return a negative index if array is empty

--- internal/recursive/binarysearch.go
+++ internal/recursive/binarysearch.go
@@ -1,8 +1,11 @@
 package recursive
 
-func BinarySearch(arr []any, value any) int {
+func BinarySearch(arr []int, value int) int {
 	if arr == nil {
 		return -1
 	}
-	return -1
+	if len(arr) == 0 {
+		return -1
+	}
+	return 0
 }
--- internal/recursive/binarysearch_test.go
+++ internal/recursive/binarysearch_test.go
@@ -15,4 +15,10 @@ func TestBinarySearch(t *testing.T) {
 
 		assert.Equal(-1, output)
 	})
+
+	t.Run(`Should return a negative index if array is empty`, func(t *testing.T) {
+		output := recursive.BinarySearch([]int{}, 3)
+
+		assert.Equal(-1, output)
+	})
 }
