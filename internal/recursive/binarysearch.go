ea63179b73d6a742274595f95eb190e8a5fa0196
RenatoValentim <rvrento28@gmail.com>
2024-04-20 02:38:22 -0300

    feat(recursive/binarysearch): return a negative index if array is nil

---
+++ internal/recursive/binarysearch.go
@@ -0,0 +1,8 @@
+package recursive
+
+func BinarySearch(arr []any, value any) int {
+	if arr == nil {
+		return -1
+	}
+	return -1
+}
---
+++ internal/recursive/binarysearch_test.go
@@ -0,0 +1,18 @@
+package recursive_test
+
+import (
+	"testing"
+
+	"github.com/RenatoValentim/algorithms/internal/recursive"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBinarySearch(t *testing.T) {
+	assert := assert.New(t)
+
+	t.Run(`Should return a negative index if array is nil`, func(t *testing.T) {
+		output := recursive.BinarySearch(nil, 3)
+
+		assert.Equal(-1, output)
+	})
+}
